7dd84bc09d6d20f81a9416f1dfc87969674065d8
Michael A. Cianfrocco <michael.a.cianfrocco@gmail.com>
2014-01-10 09:56:09 -0500

    Extended functionality of uploader
    Now allows user to specify the tilt extensions

--- make_uploader_file.py
+++ make_uploader_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 #This script will generate a text file containing a list of untilted and tilted micrographs for uploading into Appion. 
+#NOTE: Assumes linear ordering of micrographs and numbering: basename01_00.mrc, where there are two digits for micrograph number ('01').
+
 #The output file will have the following columns: 
 
 #/path/to/filename	pixelsize(meters)	binx	biny	nominalscopemag	df	HT	alphatilt
@@ -17,18 +19,18 @@
 #INPUTS ARE HERE#
 #################
 
-#NOTE: Assumes linear ordering of micrographs
-
-pwd = '/labdata/allab/michaelc/David_Dicer_data'	#Full path to micrographs
-base = '12aug01q_'					#Basename of micrographs. Leave blank if no basename.
-outfile = 'dicer_upload.txt'				#Output filename
-pixelsize = 2.18e-10					#Pixel size (IN METERS)	
-tiltangle = 55						#Tilt angle used 
+pwd = '/labdata/allab/michaelc/5merRCT/gHgLgO'	#Full path to micrographs
+base = ''					#Basename of micrographs. Leave blank if no basename.
+outfile = 'RCT_upload.txt'				#Output filename
+pixelsize = 2.16e-10					#Pixel size (IN METERS)	
+tiltangle = -50						#Tilt angle used 
 binning = 1						#Binning of micrographs (1 = no binning)
 magnification = 50000					#Magnification
 hightension = 120					#High tension of microscope (IN KILOVOLTS)
 defocus = -1e-06					#Approx. defocus (IN METERS)	
-tot = 35						#Total number of tilt pairs 
+tot = 30						#Total number of tilt pairs 
+untilt_extension = '_00'				#Indicator in filename specifying if micrograph was tilted
+tilt_extension = '_50'					#Indicator in filename specifying if micrograph was tilted
 
 #################
 #Program ########
@@ -39,10 +41,11 @@ o1 = open(outfile,'w')
 first=1
 last=tot
 
+
 while first <= last:
 
-	tilt ='%s/%s%03dt.mrc' %(pwd,base,first)
-	untilt = '%s/%s%03du.mrc' %(pwd,base,first)
+	tilt ='%s/%s%02d%s.mrc' %(pwd,base,first,tilt_extension)
+	untilt = '%s/%s%02d%s.mrc' %(pwd,base,first,untilt_extension)
 
 	o1.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' %(untilt,str(pixelsize),str(binning),str(binning),str(magnification),str(defocus),str(hightension),str(0)))
 
